a0553e5c80a9cf166e93be761f4d7a2ce0c1f73a
7-hader <hader.seven.com>
2023-10-20 01:39:04 -0500

    API consumption for cards at home

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-
-function Card() {
+const Card = ({ data }) => {
     return (
       <div className='bg-white w-56 h-60 rounded-lg cursor-pointer'>
         <figure className='relative w-full h-4/5 mb-2'>
           <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs px-3 py-0.5 m-2'>
-            Electronics
+            { data.category.name }
         </span>
           <img className='w-full h-full rounded-lg object-cover' 
-            src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' 
+            src={ data.images[0] } alt={ data.title } 
         />
           <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1 m-2'>
             +
@@ -16,10 +14,10 @@ function Card() {
         </figure>
         <p className='flex justify-between'>
           <span className='text-sm font-light'>
-            Headphones
+          { data.title }
             </span>
           <span className='text-lg font-medium'>
-            $300
+            ${ data.price }
             </span>
         </p>
       </div>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [item, setItem] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItem(data))
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          item?.map(item => (
+            <Card 
+              key={item.id}
+              data={item} 
+            />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
