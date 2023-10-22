961cb4d5d2cd994b3285371a9b22a2d24e2dd68e
7-hader <hader.seven.com>
2023-10-22 17:40:12 -0500

    refactor: fix location global context and add cart counter

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
+
 const Card = ({ data }) => {
+    const context = useContext(ShoppingCartContext)
+    
     return (
       <div className='bg-white w-56 h-60 rounded-lg cursor-pointer'>
         <figure className='relative w-full h-4/5 mb-2'>
@@ -8,7 +13,9 @@ const Card = ({ data }) => {
           <img className='w-full h-full rounded-lg object-cover' 
             src={ data.images[0] } alt={ data.title } 
         />
-          <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1 m-2'>
+          <div 
+            className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1 m-2'
+            onClick={() => context.setCount(context.count + 1)}>
             +
           </div>
         </figure>
