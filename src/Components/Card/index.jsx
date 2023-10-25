043616270ed2952e93cf20b0f62858b89af7d8dd
7-hader <hader.seven.com>
2023-10-25 02:05:54 -0500

    feat: add icons to components

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -16,7 +16,9 @@ const Card = ({ data }) => {
           <div 
             className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1 m-2'
             onClick={() => context.setCount(context.count + 1)}>
-            +
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
           </div>
         </figure>
         <p className='flex justify-between'>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -5,7 +5,12 @@ const ProductDetail = () => {
             border-black rounded-lg bg-white w-[360px] h-[calc(100vh-68px)]">
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>x</div>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+
+                </div>
             </div>
         </aside>
     )
